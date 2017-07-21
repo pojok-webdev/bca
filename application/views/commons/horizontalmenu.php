1181235c1d1abe2859d82d7e0d0ae3617296bfc6
pujie <pw.prayitno@gmail.com>
2017-07-21 09:51:08 +0700

    change menu, records instead of import excel

--- application/views/commons/horizontalmenu.php
+++ application/views/commons/horizontalmenu.php
@@ -2,7 +2,7 @@
 <div class="col-md-2 bootstrap-admin-col-left">
     <ul class="nav navbar-collapse collapse bootstrap-admin-navbar-side">
         <li class="<?php echo ($feedData==='processimport')?'active':'';?>">
-            <a href="/records"><i class="glyphicon glyphicon-chevron-right"></i> Import Excel</a>
+            <a href="/records"><i class="glyphicon glyphicon-chevron-right"></i> Records</a>
         </li>
         <li class="<?php echo ($feedData==='settings')?'active':'';?>">
             <a href="/settings"><i class="glyphicon glyphicon-chevron-right"></i> Setting</a>
